55db6e9a6e82060ef4f4763a72eb38dcb3a6a38f
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-10-18 20:14:53 +0100

    first code Nombre

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+import Nombre.Nombre;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        Nombre N = new Nombre(5);
+        N.afficher();
     }
 }
--- src/Nombre/Nombre.java
+++ src/Nombre/Nombre.java
@@ -7,6 +7,16 @@ public class Nombre {
         Num = n;
     }
 
-    
+    private int inc() {
+        return Num = Num + 1;
+    }
+
+    private int dec() {
+        return Num = Num - 1;
+    }
+
+    public void afficher() {
+        System.out.println("la valeur inc est " + inc() + " et la valeur dec est" + dec());
+    }
 
 }
